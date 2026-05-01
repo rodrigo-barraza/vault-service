049b62cd1eb18aa3bc56ca2e85c415f01990a41d
Rodrigo Barraza <hello@rod.dev>
2026-05-01 00:06:52 -0700

    chore: standardize project linting and formatting configuration with Prettier and ESLint upgrades

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,33 @@
 import js from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
+  prettierConfig,
   {
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
       globals: {
-        console: "readonly",
-        process: "readonly",
-        URL: "readonly",
-        URLSearchParams: "readonly",
-        AbortSignal: "readonly",
-        fetch: "readonly",
-        setInterval: "readonly",
-        clearInterval: "readonly",
-        setTimeout: "readonly",
+        ...globals.node,
       },
     },
     rules: {
-      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "no-console": "off",
+      "prefer-const": "error",
+      "no-var": "error",
     },
+  },
+  {
+    ignores: ["node_modules/"],
   },
 ];
